6ee2a506b563740e039a34abc0664f4b7d8b9009
cbellec <corentin.bellec@etudiant.univ-lr.fr>
2021-12-08 11:12:01 +0100

    🐛 FIX: merge conflit

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
-
-<<<<<<< HEAD
 from app.routes.route_client import router_client as router_client
 
 app = FastAPI()
 
 app.include_router(router_client, tags=["v1"], prefix="/v1")
-=======
+
 app = FastAPI()
 
->>>>>>> 09d56879ab7c5bba9dd12c13f9983ed9be40fa86
 
 @app.get("/")
 def read_root():
@@ -19,4 +16,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
-    
