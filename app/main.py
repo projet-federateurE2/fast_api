86bf36c023768e2d3eac764126d8ac9ed589ccae
p0lycarpio <arsene.reymond@free.fr>
2021-12-10 11:07:21 +0100

    docker stack functionnal

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes.route_client import router_client as router_client
+from routes.route_client import router_client
 
 app = FastAPI()
 
@@ -7,7 +7,7 @@ app.include_router(router_client, tags=["v1"], prefix="/v1")
 
 @app.get("/")
 def read_root():
-    return {"Hello": "world"}
+    return {"Hello": "aa"}
 
 
 @app.get("/items/{item_id}")
