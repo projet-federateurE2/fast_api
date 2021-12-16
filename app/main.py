5fc5ebefc7125039b0d71a64d8ab5e782e35eaa3
Théo Triou <theo.triou@etudiant.univ-lr.fr>
2021-12-16 11:59:33 +0100

    update route

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.routes.route_proprietaire import router_proprietaire
+from app.routes.route_artisan import router_artisan
 
 app = FastAPI()
 
 # app.include_router(router_proprietaire, tags=["test"], prefix="/test")
 
 app.include_router(router_proprietaire, tags=["v1"], prefix="/v1")
+
+app.include_router(router_artisan, tags=["v1"], prefix="/v1")
